aa8503e22e409144151bfa43c98b93d0a7d33398
Mathias Lengler <mathmatze@googlemail.com>
2017-06-02 23:05:18 +0200

    add grid json

--- src/main/scala/de/htwg/sa/SolveService/routes/SimpleRoutes.scala
+++ src/main/scala/de/htwg/sa/SolveService/routes/SimpleRoutes.scala
@@ -6,16 +6,16 @@ import akka.http.scaladsl.server.Route
 import de.htwg.sa.SolveService.services.GridService
 
 /**
-  * Routes can be defined in separated classes like shown in here
-  */
+ * Routes can be defined in separated classes like shown in here
+ */
 trait SimpleRoutes {
 
   // This `val` holds one route (of possibly many more that will be part of your Web App)
   lazy val simpleRoutes: Route =
     path("hello") { // Listens to paths that are exactly `/hello`
-        get {
-          complete("Hi!")
-        }
+      get {
+        complete("Hi!")
+      }
     } ~
       path("solve") {
         GridService.route
--- src/main/scala/de/htwg/sa/SolveService/services/GridService.scala
+++ src/main/scala/de/htwg/sa/SolveService/services/GridService.scala
@@ -1,17 +1,17 @@
 package de.htwg.sa.SolveService.services
 
-import akka.http.scaladsl.server.{Directives, Route}
+import akka.http.scaladsl.server.{ Directives, Route }
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import spray.json._
+import minesweeper.model.impl.{ Cell, Grid, GridFactory }
+import minesweeper.model.{ ICell, ICellMutable, IGrid }
 
-import minesweeper.model.impl.Cell
-import minesweeper.model.ICell
+import scala.collection.JavaConverters._
 
+object MinesweeperProtocol extends DefaultJsonProtocol {
 
-object MyJsonProtocol extends DefaultJsonProtocol {
-
-  implicit object CellJsonFormat extends RootJsonFormat[Cell] {
-    def write(c: Cell): JsObject = {
+  implicit object CellJsonFormat extends RootJsonFormat[ICell] {
+    def write(c: ICell): JsObject = {
       JsObject(
         "hasMine" -> JsBoolean(c.isMine),
         "isFlagged" -> JsBoolean(c.isFlag),
@@ -24,14 +24,13 @@ object MyJsonProtocol extends DefaultJsonProtocol {
       )
     }
 
-    def read(value: JsValue): Cell = {
-      value.asJsObject.getFields("hasMine", "isFlagged", "isRevealed", "position", "surroundingMines")
-      match {
+    def read(value: JsValue): ICell = {
+      value.asJsObject.getFields("hasMine", "isFlagged", "isRevealed", "position", "surroundingMines") match {
         case Seq(JsBoolean(hasMine),
-        JsBoolean(isFlagged),
-        JsBoolean(isRevealed),
-        position: JsObject,
-        JsNumber(surroundingMines)) =>
+          JsBoolean(isFlagged),
+          JsBoolean(isRevealed),
+          position: JsObject,
+          JsNumber(surroundingMines)) =>
           position.getFields("row", "col") match {
             case Seq(JsNumber(row), JsNumber(col)) =>
               val state = if (isRevealed) {
@@ -42,10 +41,40 @@ object MyJsonProtocol extends DefaultJsonProtocol {
                 ICell.State.CLOSED
               }
               new Cell(row.toInt, col.toInt, state, surroundingMines.toInt, hasMine)
-            case _ => throw DeserializationException("Cell expected")
+            case _ => throw DeserializationException("Position expected")
           }
         case _ => throw DeserializationException("Cell expected")
       }
+    }
+  }
+
+  implicit object GridJsonFormat extends RootJsonFormat[IGrid[ICell]] {
+    def write(g: IGrid[ICell]): JsArray = {
+      implicit val p = MinesweeperProtocol
+
+      val cells = g.getNestedCells
+
+      JsArray(cells.asScala
+        .map(_.asScala)
+        .map(_.map(cell => cell.toJson))
+        .map(row => JsArray(row.to[Vector]))
+        .to[Vector])
+    }
+
+    def read(value: JsValue): IGrid[ICell] = {
+      val cells = value match {
+        case JsArray(outer_vec: Vector[JsValue]) =>
+          outer_vec.map {
+            case JsArray(inner_vec: Vector[JsValue]) =>
+              inner_vec.map(_.convertTo[ICell])
+            case _ => throw DeserializationException("Rows expected")
+          }
+        case _ => throw DeserializationException("Grid expected")
+      }
+
+      val mines = cells.map(_.count(_.isMine)).sum[Int]
+
+      new Grid[ICell](cells.map(_.toArray).toArray, mines)
     }
   }
 
@@ -53,17 +82,18 @@ object MyJsonProtocol extends DefaultJsonProtocol {
 
 // collect your json format instances into a support trait:
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
-  implicit val cellFormat: RootJsonFormat[Cell] = MyJsonProtocol.CellJsonFormat
+  implicit val cellFormat = MinesweeperProtocol.CellJsonFormat
+  implicit val gridFormat = MinesweeperProtocol.GridJsonFormat
 }
 
 object GridService extends Directives with JsonSupport {
   val route: Route =
-      get {
-        complete(new Cell(1, 2))
-      } ~
-        post {
-          entity(as[Cell]) { cell =>
-            complete(cell.mkString())
-          }
+    get {
+      complete(new GridFactory(2, 2).getGrid)
+    } ~
+      post {
+        entity(as[IGrid[ICell]]) { grid =>
+          complete(grid.toString)
         }
+      }
 }
