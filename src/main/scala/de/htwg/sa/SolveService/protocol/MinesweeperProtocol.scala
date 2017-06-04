9314cb270793399b4685188ea81a6da38bce75d9
Mathias Lengler <mathmatze@googlemail.com>
2017-06-04 20:02:33 +0200

    implement solver api

---
+++ src/main/scala/de/htwg/sa/SolveService/protocol/MinesweeperProtocol.scala
@@ -0,0 +1,103 @@
+package de.htwg.sa.SolveService.protocol
+
+import minesweeper.model.impl.{Cell, Grid}
+import minesweeper.model.{ICell, IGrid}
+import minesweeper.solverplugin.impl.jacop.JacopSolver
+import minesweeper.solverplugin.impl.jacop.JacopSolver.SolveResult
+import spray.json._
+
+import scala.collection.JavaConverters._
+
+
+object MinesweeperProtocol extends DefaultJsonProtocol {
+  case class Position(row: Int, col: Int)
+
+  implicit class CellExtension(cell: ICell) {
+    def getPosition = Position(cell.getRow, cell.getCol)
+  }
+
+  implicit val positionFormat: RootJsonFormat[Position] = jsonFormat2(Position)
+
+  implicit object CellJsonFormat extends RootJsonFormat[ICell] {
+    def write(c: ICell): JsObject = {
+      JsObject(
+        "hasMine" -> JsBoolean(c.isMine),
+        "isFlagged" -> JsBoolean(c.isFlag),
+        "isRevealed" -> JsBoolean(c.isOpened),
+        "position" -> c.getPosition.toJson,
+        "surroundingMines" -> JsNumber(c.getMines)
+      )
+    }
+
+    def read(value: JsValue): ICell = {
+      value.asJsObject.getFields("hasMine", "isFlagged", "isRevealed", "position", "surroundingMines") match {
+        case Seq(JsBoolean(hasMine),
+        JsBoolean(isFlagged),
+        JsBoolean(isRevealed),
+        position: JsObject,
+        JsNumber(surroundingMines)) =>
+          val p = position.convertTo[Position]
+          val state = if (isRevealed) {
+            ICell.State.OPENED
+          } else if (isFlagged) {
+            ICell.State.FLAG
+          } else {
+            ICell.State.CLOSED
+          }
+          new Cell(p.row, p.col, state, surroundingMines.toInt, hasMine)
+        case _ => throw DeserializationException("Cell expected")
+      }
+    }
+  }
+
+  implicit object SolveResultJsonFormat extends RootJsonFormat[SolveResult] {
+    def write(r: SolveResult): JsObject = {
+      JsObject(
+        "mineProbabilities" -> JsArray(
+          r.cellProb.asScala.map { case (cell, probability) =>
+            JsObject(
+              "cell" -> cell.getPosition.toJson,
+              "probability" -> JsNumber(probability)
+            )
+          }.toVector),
+        "clears" -> JsArray(r.clearsToOpen.asScala.map(_.getPosition.toJson).toVector),
+        "mines" -> JsArray(r.minesToFlag.asScala.map(_.getPosition.toJson).toVector)
+      )
+    }
+
+    def read(value: JsValue): SolveResult = {
+      throw new UnsupportedOperationException
+    }
+  }
+
+  implicit object GridJsonFormat extends RootJsonFormat[IGrid[ICell]] {
+    def write(g: IGrid[ICell]): JsArray = {
+      implicit val p = MinesweeperProtocol
+
+      val cells = g.getNestedCells
+
+      JsArray(cells.asScala
+        .map(_.asScala)
+        .map(_.map(cell => cell.toJson))
+        .map(row => JsArray(row.to[Vector]))
+        .to[Vector])
+    }
+
+    def read(value: JsValue): IGrid[ICell] = {
+      val cells = value match {
+        case JsArray(outer_vec: Vector[JsValue]) =>
+          outer_vec.map {
+            case JsArray(inner_vec: Vector[JsValue]) =>
+              inner_vec.map(_.convertTo[ICell])
+            case _ => throw DeserializationException("Rows expected")
+          }
+        case _ => throw DeserializationException("Grid expected")
+      }
+
+      val mines = cells.map(_.count(_.isMine)).sum[Int]
+
+      new Grid[ICell](cells.map(_.toArray).toArray, mines)
+    }
+  }
+
+}
--- src/main/scala/de/htwg/sa/SolveService/services/GridService.scala
+++ src/main/scala/de/htwg/sa/SolveService/services/GridService.scala
@@ -2,88 +2,17 @@ package de.htwg.sa.SolveService.services
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.server.{Directives, Route}
+import de.htwg.sa.SolveService.protocol.MinesweeperProtocol
 import minesweeper.model.impl.{Cell, Grid, GridFactory}
 import minesweeper.model.{ICell, IGrid}
 import spray.json._
 
-import scala.collection.JavaConverters._
-
-object MinesweeperProtocol extends DefaultJsonProtocol {
-
-  implicit object CellJsonFormat extends RootJsonFormat[ICell] {
-    def write(c: ICell): JsObject = {
-      JsObject(
-        "hasMine" -> JsBoolean(c.isMine),
-        "isFlagged" -> JsBoolean(c.isFlag),
-        "isRevealed" -> JsBoolean(c.isOpened),
-        "position" -> JsObject(
-          "row" -> JsNumber(c.getRow),
-          "col" -> JsNumber(c.getCol)
-        ),
-        "surroundingMines" -> JsNumber(c.getMines)
-      )
-    }
-
-    def read(value: JsValue): ICell = {
-      value.asJsObject.getFields("hasMine", "isFlagged", "isRevealed", "position", "surroundingMines") match {
-        case Seq(JsBoolean(hasMine),
-          JsBoolean(isFlagged),
-          JsBoolean(isRevealed),
-          position: JsObject,
-          JsNumber(surroundingMines)) =>
-          position.getFields("row", "col") match {
-            case Seq(JsNumber(row), JsNumber(col)) =>
-              val state = if (isRevealed) {
-                ICell.State.OPENED
-              } else if (isFlagged) {
-                ICell.State.FLAG
-              } else {
-                ICell.State.CLOSED
-              }
-              new Cell(row.toInt, col.toInt, state, surroundingMines.toInt, hasMine)
-            case _ => throw DeserializationException("Position expected")
-          }
-        case _ => throw DeserializationException("Cell expected")
-      }
-    }
-  }
-
-  implicit object GridJsonFormat extends RootJsonFormat[IGrid[ICell]] {
-    def write(g: IGrid[ICell]): JsArray = {
-      implicit val p = MinesweeperProtocol
-
-      val cells = g.getNestedCells
-
-      JsArray(cells.asScala
-        .map(_.asScala)
-        .map(_.map(cell => cell.toJson))
-        .map(row => JsArray(row.to[Vector]))
-        .to[Vector])
-    }
-
-    def read(value: JsValue): IGrid[ICell] = {
-      val cells = value match {
-        case JsArray(outer_vec: Vector[JsValue]) =>
-          outer_vec.map {
-            case JsArray(inner_vec: Vector[JsValue]) =>
-              inner_vec.map(_.convertTo[ICell])
-            case _ => throw DeserializationException("Rows expected")
-          }
-        case _ => throw DeserializationException("Grid expected")
-      }
-
-      val mines = cells.map(_.count(_.isMine)).sum[Int]
-
-      new Grid[ICell](cells.map(_.toArray).toArray, mines)
-    }
-  }
-
-}
 
 // collect your json format instances into a support trait:
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val cellFormat = MinesweeperProtocol.CellJsonFormat
   implicit val gridFormat = MinesweeperProtocol.GridJsonFormat
+  implicit val solveResultFormat = MinesweeperProtocol.SolveResultJsonFormat
 }
 
 object GridService extends Directives with JsonSupport {
@@ -93,7 +22,13 @@ object GridService extends Directives with JsonSupport {
     } ~
       post {
         entity(as[IGrid[ICell]]) { grid =>
-          complete(grid)
+          complete(getPostResponse(grid))
         }
       }
+
+  private def getPostResponse(grid: IGrid[ICell]) = {
+    import minesweeper.solverplugin.impl.jacop.JacopSolver
+
+    JacopSolver.buildSolveResult(grid)
+  }
 }
