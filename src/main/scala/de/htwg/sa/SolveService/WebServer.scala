fc7bb722c65c87d56dc70d11d8e5bbb7fa32179e
Mathias Lengler <mathmatze@googlemail.com>
2017-06-27 06:26:13 +0200

    listen on anycast and dont expect open stdin to run server

--- src/main/scala/de/htwg/sa/SolveService/WebServer.scala
+++ src/main/scala/de/htwg/sa/SolveService/WebServer.scala
@@ -19,13 +19,9 @@ object WebServer extends Directives with SimpleRoutes {
 
     PropertyConfigurator.configure("log4j.properties")
 
-    val bindingFuture = Http().bindAndHandle(routes, "localhost", 8080)
+    val bindingFuture = Http().bindAndHandle(routes, "0.0.0.0", 8080)
 
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-    StdIn.readLine() // let it run until user presses return
-    bindingFuture
-      .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
+    println(s"Server online at http://0.0.0.0:8080/")
   }
 
   // Here you can define all the different routes you want to have served by this web server
