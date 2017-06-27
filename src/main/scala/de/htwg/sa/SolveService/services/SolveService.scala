0578b78438420ed651128251d876303382a4619a
Mathias Lengler <mathmatze@googlemail.com>
2017-06-27 08:50:54 +0200

    clean up

--- src/main/scala/de/htwg/sa/SolveService/services/SolveService.scala
+++ src/main/scala/de/htwg/sa/SolveService/services/SolveService.scala
@@ -1,17 +1,15 @@
 package de.htwg.sa.SolveService.services
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.server.{ Directives, Route }
+import akka.http.scaladsl.server.{Directives, Route}
 import de.htwg.sa.SolveService.WebServer
 import de.htwg.sa.SolveService.protocol.MinesweeperProtocol
-import minesweeper.model.impl.GridFactory
-import minesweeper.model.{ ICell, IGrid }
+import minesweeper.model.{ICell, IGrid}
 import minesweeper.solverplugin.impl.jacop.JacopSolver
 import spray.json._
 
 import scala.concurrent.Future
 
-// collect your json format instances into a support trait:
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val cellFormat = MinesweeperProtocol.CellJsonFormat
   implicit val gridFormat = MinesweeperProtocol.GridJsonFormat
